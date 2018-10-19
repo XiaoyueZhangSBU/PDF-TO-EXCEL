77fe557b32bf2b322a91775073291f1294f4deee
XiaoyueZhangSBU <31393042+XiaoyueZhangSBU@users.noreply.github.com>
2018-10-19 11:05:02 -0400

    Update R-pdftoexcel.R

--- R-pdftoexcel.R
+++ R-pdftoexcel.R
@@ -1,5 +1,10 @@
 install.packages("pdftools")
+install.packages("stringr")
+
 library(pdftools)
+library(stringr)
+
+#input data
 download.file("file:///Users/moon/Desktop/asmsdirectoryofmembers.pdf","asmsdirectoryofmembers.pdf")
 text = pdf_text("asmsdirectoryofmembers.pdf")   ##read pdf
 text1 = strsplit(text,"\n")   ##wrap new line
@@ -13,13 +18,13 @@ for(i in 1:32) {
 }
 
 
-##展平数据
+##unlist data
 text3 = as.character(unlist(text2))
 
-##词与词之间空格超过2个，则换行
+##wrap new line once exist two blanck space
 text4 = strsplit(text3, "  ")
 
-##剔除无数据成分
+##remove NA
 for (n in 1:length(text4)){
 a = which(text4[[n]] == "")
 for (i in 1:length(a)){
@@ -28,7 +33,7 @@ for (i in 1:length(a)){
 text4[[n]] = na.omit(text4[[n]])
 }
 
-##所有数据整理为一列
+##list all information
 text5 = list()
 
 for (j in 1:length(text4)) {
@@ -38,21 +43,19 @@ for (j in 1:length(text4)) {
 }
 
 
-##选取带有“电话”的行信息
+##choose "Tel"
 text_tel = grep("Tel:", text5)
 tel = text5[text_tel]
 
-##选取带有“邮箱”的行信息
+##choose Email
 text_email = grep("@", text5)
 email = text5[text_email]
 
-install.packages("stringr")
-library(stringr)
+
 text_name = list()
 for (t in 1:length(text5)) {
   text_name[t] = grep(str_to_upper(text5))
 }
-
 
 
 page_rows = c(0, which(doc1 == "page"), length(doc1))
@@ -61,11 +64,10 @@ for ( i in 1:(length(page_rows)-1)) {
   doc = c(doc,c(doc1[(page_rows[i]+1):page_rows[i+1]], doc2[(page_rows[i]+1):page_rows[i+1]]))}
 doc = doc[doc != "page"]
 
+
 ###write into xlsx
 xlsxfile = paste(1:32, ".xlsx", sep=" ")
 for (i in 1:32) {
   text[[i]] = paste("/User/moon/Desktop", xlsxfile[i], sep = " ")
   write.csv(data.list2[[i]], text2)
 }
-
-
